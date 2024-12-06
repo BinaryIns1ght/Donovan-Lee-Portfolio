6ab3d1363bbefde62406983990ad9fc2c2708d6c
BinaryIns1ght <79720466+BinaryIns1ght@users.noreply.github.com>
2024-12-05 16:07:14 -0800

    Started adding an overlay navbar

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function navbarOn() {
+    document.getElementById("navbar").style.width = "30%";
+    document.getElementById("navbarBG").style.width = "100%";
+}
+
+function navbarOff() {
+    document.getElementById("navbar").style.width = "0%";
+    document.getElementById("navbarBG").style.width = "0%";
+}
