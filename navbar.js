6fb1d1c978a79a524057acabfed2f347445c5f4d
BinaryIns1ght <79720466+BinaryIns1ght@users.noreply.github.com>
2024-12-07 15:42:06 -0800

    Making Some Starting Changes To The Portfolio

--- navbar.js
+++ navbar.js
@@ -1,9 +1,7 @@
 function navbarOn() {
     document.getElementById("navbar").style.width = "30%";
-    document.getElementById("navbarBG").style.width = "100%";
 }
 
 function navbarOff() {
     document.getElementById("navbar").style.width = "0%";
-    document.getElementById("navbarBG").style.width = "0%";
 }
